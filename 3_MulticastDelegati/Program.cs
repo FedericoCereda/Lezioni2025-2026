d2640a6b9e07d811ce32f4ab58ca1ef06aa9c0f6
Federico <federico.cereda@issgreppi.it>
2025-10-15 08:24:25 +0200

    Prova

---
+++ 3_MulticastDelegati/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    public delegate void del(string s);
+    public static void Hello(string s)
+    {
+        Console.WriteLine($"Hello {s}");
+    }
+    public static void Goobye (string s)
+    {
+        Console.WriteLine($"Goobye {s}");
+    }
+    private static void Main(string[] args)
+    {
+        del a, b, c, d, k;
+        a = new del(Hello);
+        b = Goobye;
+    }
+}
